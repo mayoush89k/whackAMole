785b46a0d0d55cf75b0fb138ff15e2f59497542a
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2024-10-04 21:42:43 +0300

    uploading games

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,114 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react";
+import "./App.css";
+import moleImg from "./assets/mole-removebg.png";
+import whackedMoleImg from "./assets/whacked_mole-removebg-preview.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mole, setMole] = useState(["hidden", "hidden", "hidden"]);
+  const [counter, setCounter] = useState(10);
+  const img1Ref = useRef(null);
+  const img2Ref = useRef(null);
+  const img3Ref = useRef(null);
+  const [isClickedInside, setIsClickedInside] = useState(false);
+
+  function moveMole(moleNumber) {
+    const newMole = [...mole];
+    newMole[moleNumber] = "visible";
+    setMole(newMole);
+    setIsClickedInside(false)
+    
+    // Use a timeout to remove the moving class after animation
+    setTimeout(() => {
+      const moleClass = `.mole${moleNumber + 1}`;
+      const currentMole = document.querySelector(moleClass);
+      if (currentMole) {
+        const direction =
+        moleNumber % 2 === 0 ? "movingMoleUp" : "movingMoleDown";
+        currentMole.classList.add(direction);
+        setTimeout(() => {
+          currentMole.classList.remove("movingMole");
+          const newMole = [...mole];
+          newMole[moleNumber] = "hidden";
+          setMole(newMole);
+          setIsClickedInside(false)
+        }, 5000);
+      }
+    }, 0);
+  }
+
+  function handleClick2() {
+    const interval = 6000; // Interval between each move (1 second)
+    let movesLeft = counter;
+
+    const moveMoleWithDelay = () => {
+      if (movesLeft > 0) {
+        const random = Math.floor(Math.random() * 3);
+        moveMole(random);
+        movesLeft--;
+        setCounter(movesLeft);
+        setTimeout(moveMoleWithDelay, interval);
+      } else {
+        setCounter(10); // Reset the counter after all moves are completed
+      }
+    };
+
+    moveMoleWithDelay();
+  }
+
+  const handleClick = (event) => {
+    const images = [img1Ref.current, img2Ref.current, img3Ref.current];
+    for (let img of images) {
+      if (img.classList[1] == "visible") {
+        const rect = img.getBoundingClientRect();
+
+        const clickX = event.clientX;
+        const clickY = event.clientY;
+
+        const isInside =
+          clickX >= rect.left &&
+          clickX <= rect.right &&
+          clickY >= rect.top &&
+          clickY <= rect.bottom;
+
+        setIsClickedInside(isInside);
+        break;
+      }
+    }
+  };
 
   return (
     <>
+      <button onClick={handleClick2}>Start</button>
+      <div>moves Left: {counter}</div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {isClickedInside ? (
+          <p>Clicked inside the image!</p>
+        ) : (
+          <p>Clicked outside the image!</p>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div onClick={handleClick} className="container">
+        <img
+          className={`mole1 ${mole[0]} ${isClickedInside ? 'whacked' : '' }`}
+          src={isClickedInside ? whackedMoleImg : moleImg}
+          alt="Mole"
+          ref={img1Ref}
+          ></img>
+        <img
+          className={`mole2 ${mole[1]} ${isClickedInside ? 'whacked' : '' }`}
+          src={isClickedInside ? whackedMoleImg : moleImg}
+          alt="Mole"
+          ref={img2Ref}
+          ></img>
+        <img
+          className={`mole3 ${mole[2]} ${isClickedInside ? 'whacked' : '' }`}
+          src={isClickedInside ? whackedMoleImg : moleImg}
+          alt="Mole"
+          ref={img3Ref}
+        ></img>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
